d8c0b35f62b28be46dc4b7afb00d18109be28d59
Parabeac-Cloud <admin@parabeac.com>
2023-06-14 15:49:14 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/salem_colors.g.dart
@@ -0,0 +1,56 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class SalemColors {
+
+static const Color title = Color(0xff0a0f0d);
+
+static const Color body = Color(0xff727272);
+
+static const Color heading = Color(0xff1c1f34);
+
+static const Color body2 = Color(0xff6c757d);
+
+static const Color background = Color(0xfff6f7f9);
+
+static const Color primary = Color(0xff5f60b9);
+
+static const Color white = Color(0xffffffff);
+
+static const Color border = Color(0xffebebeb);
+
+static const Color main = Color(0xff5f60b9);
+
+static const Color black = Color(0xff1c1f34);
+
+static const Color bg = Color(0xff6c757d);
+
+static const Color border2 = Color(0xffebebeb);
+
+static const Color systemColorsSCLWhite = Color(0xffffffff);
+
+static const Color main2 = Color(0xff5f60b9);
+
+static const Color border3 = Color(0xffebebeb);
+
+static const Color heading2 = Color(0xff1c1f34);
+
+static const Color body3 = Color(0xff6c757d);
+
+static const Color background2 = Color(0xfff6f7f9);
+
+static const Color primary2 = Color(0xff5f60b9);
+
+static const Color white2 = Color(0xffffffff);
+
+static const Color black2 = Color(0xff1c1f34);
+
+static const Color bg2 = Color(0xff6c757d);
+
+static const Color border4 = Color(0xffebebeb);
+}
---
+++ lib/theme/salem_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class SalemTextStyles {
+
+static const TextStyle metadata03 = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Mulish',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
